b2b7dcec504533d8792515dfef9f25883daeab46
Reicsen <reicsen@gmail.com>
2025-06-03 20:41:55 +0200

    Konstruktory Kn, Kmn, Cn; sumowanie grafów w nowy graf

--- build/BuildGraphs.hs
+++ build/BuildGraphs.hs
@@ -1,6 +1,6 @@
 -- na razie tak, żeby było prościej pracować nad resztą rzeczy; na koniec można poszukać szybszej wersji (ta zbyt wymagająca dla dużych grafów gęstych)
 
-module BuildGraphs (buildGraphStandard,buildGraphLight) where
+module BuildGraphs (buildGraphStandard,buildGraphLight,buildKn,buildKmn,buildCn,sum2Graphs,sumGraphs) where
 
 import qualified Data.Map.Strict as Map
 import Data.List
@@ -54,3 +54,24 @@ buildGraphStandard n edges = if n<=0 then Map.fromList []
 
 buildGraphLight n edges = if n<=0 then Map.fromList []      -- for first-coordinate-sorted edge lists - NEEDS BOTH (a,b) and (b,a) to be part of the list
                     else Map.fromList (buildAdjListsLight n [(i,[]) | i <- [1..n]] edges)
+
+buildKn n = Map.fromList([(i,[j | j <- [1..n], j/=i]) | i <- [1..n]])
+
+buildKmn m n = if m<=0 || n<0 then Map.fromList []
+                else Map.fromList([(i,[j | j <- [1..m], j/=i]) | i <- [1..m]]++[(i,[j | j <- [m+1..m+n], j/=i]) | i <- [m+1..m+n]])
+
+buildCn n = if n<=0 then Map.fromList []
+            else if n==1 then Map.fromList [(1,[])]
+            else if n==2 then Map.fromList [(1,[2]),(2,[1])]
+            else Map.fromList(((1,[2,n]):[(i,[(i-1),(i+1)]) | i <- [2..n-1]])++[(n,[1,(n-1)])])
+
+sum2Graphs g1 g2 = let  l1 = (Map.toList g1)
+                        l2 = (Map.toList g2) 
+                        n = (length l1) in
+                    Map.fromList(l1++(map (\(a,b) -> (a+n,(map (+n) b))) l2))
+
+
+sumGraphs [] = Map.fromList []
+sumGraphs [g] = g
+sumGraphs [g1,g2] = sum2Graphs g1 g2
+sumGraphs (g:gs) = sum2Graphs g (sumGraphs gs)
--- build/TestBuildGraph.hs
+++ build/TestBuildGraph.hs
@@ -1,13 +1,13 @@
 import qualified Data.Map.Strict as Map
-import BuildGraphs (buildGraphStandard,buildGraphLight)
+import BuildGraphs (buildGraphStandard,buildGraphLight,buildKn,buildKmn,buildCn,sum2Graphs,sumGraphs)
 
 test 1 = buildGraphStandard 0 [(1,3),(8,1),(0,1)] -- should be empty
 
 
-test 2 = buildGraphStandard 5 [(i,j) | i <- [1..5], j <- [i..5]]    -- C5
+test 2 = buildGraphStandard 5 [(i,j) | i <- [1..5], j <- [i..5]]    -- K5
 
 
-test 3 = buildGraphStandard 5 [(i,j) | i <- [1..7], j <- [i..7]]    -- still C5, edges containing vertices with id>5 should be removed by the function
+test 3 = buildGraphStandard 5 [(i,j) | i <- [1..7], j <- [i..7]]    -- still K5, edges containing vertices with id>5 should be removed by the function
 
 
 test 4 = buildGraphStandard 100 ([(2*i,2*j) | i <- [1..50], j <- [i..50]]++[(2*i-1,2*j-1) | i <- [1..50], j <- [i..50]])
@@ -15,7 +15,7 @@ test 4 = buildGraphStandard 100 ([(2*i,2*j) | i <- [1..50], j <- [i..50]]++[(2*i
 
 
 test 5 = buildGraphStandard 10000 ([(2*i,2*j) | i <- [1..5000], j <- [i..5000]]++[(2*i-1,2*j-1) | i <- [1..5000], j <- [i..5000]])
--- should create 5000,5000, where one group contans all odd ids and the second group contains all even ids (all ids are <=10000) - tests creation of big graphs
+-- should create K5000,5000, where one group contans all odd ids and the second group contains all even ids (all ids are <=10000) - tests creation of big graphs
 
 test _ = error"Nieznany numer testu"
 
@@ -24,10 +24,10 @@ test _ = error"Nieznany numer testu"
 test2 1 = buildGraphLight 0 [(1,3),(8,1),(0,1)] -- should be empty
 
 
-test2 2 = buildGraphLight 5 [(i,j) | i <- [1..5], j <- [1..5]]    -- C5
+test2 2 = buildGraphLight 5 [(i,j) | i <- [1..5], j <- [1..5]]    -- K5
 
 
-test2 3 = buildGraphLight 5 [(i,j) | i <- [1..7], j <- [1..7]]    -- still C5, edges containing vertices with id>5 should be removed by the function
+test2 3 = buildGraphLight 5 [(i,j) | i <- [1..7], j <- [1..7]]    -- still K5, edges containing vertices with id>5 should be removed by the function
 
 
 test2 4 = buildGraphLight 100 [(i,j) | i <- [1..100], j <- [1..100], (mod i 2)==(mod j 2)]
@@ -36,5 +36,12 @@ test2 4 = buildGraphLight 100 [(i,j) | i <- [1..100], j <- [1..100], (mod i 2)==
 
 test2 _ = error"Nieznany numer testu"
 
-{-- budowanie dużych grafów gęstych jest bardzo długie, bo baaaardzo dużo krawędzi
-dodam konstruktory np. Cn, Km,n itp., żeby można było szybko je generować dla testów itp. --}
+testKn = take 3 (Map.toList(buildKn 100000))
+
+testKmn = take 3 (Map.toList(buildKmn 2 100000))
+
+testCn = take 10 (Map.toList(buildCn 1000000))
+
+testSum2 = sum2Graphs (buildCn 10) (buildKn 5)
+
+testSum = sumGraphs [(buildCn n) | n <- [1..10]]
